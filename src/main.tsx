0b36c22ee4448dfaa0008b95e746237b90dd4d68
Leonardo Pereira <leonardo_human@hotmail.com>
2023-08-30 11:08:45 -0300

    separated components

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import "./components/global.css";
-import twitterLogo from "./assets/logo-twitter.svg";
-import { House, Hash, Bell, BookmarkSimple, Envelope, FileText, User, DotsThreeCircle, Sparkle } from "phosphor-react";
+import "./global.css";
+
+import { Tweet } from './components/Tweet';
+import { Sidebar } from './components/Sidebar';
+import { Header } from './components/Header';
+import { Separator } from './components/Separator';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar">
-        <img className='logo' src={twitterLogo} alt="Logo Twitter" />
-
-        <nav className='main-navigation'>
-          <a className='active' href=""> <House weight='fill'/> Home</a>
-          <a href=""><Hash /> Explore</a>
-          <a href=""><Bell /> Notifications</a>
-          <a href=""><Envelope /> Messages</a>
-          <a href=""><BookmarkSimple />Bookmarks</a>
-          <a href=""><FileText />Lists</a>
-          <a href=""><User />Profile</a>
-          <a href=""><DotsThreeCircle />More</a>
-        </nav>
-
-        <button className='new-tweet'>Tweet</button>
-      </aside>
-
+      <Sidebar />
+      
       <div className="content">
         <main className='timeline'>
-          <div className="timeline-header">
-            Home
-            <Sparkle />
-          </div>
+          
+          <Header title="Home"/>
 
           <form className='new-tweet-form'>
             <label htmlFor="tweet">
@@ -41,7 +27,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <button type="submit">Tweet</button>
           </form>
 
-          
+          <Separator />
+
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+
+
         </main>
       </div>
     </div>
